065e0d1276e71aed27da201e8cdc32af8a7855e0
NicoTheRicoo <nicotherico@gmail.com>
2024-07-11 16:21:01 -0400

    started js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let var1 = 0;
+let var1 = 7;
 let var2 = 0;
 let sign;
 
@@ -6,6 +6,29 @@ let sign;
 const buttons = document.querySelectorAll("button");
     buttons.forEach((Buttons) => {
         Buttons.addEventListener("click", () => {
-            
+        if(Buttons.className === "digit"){
+            updateDisplay(var1);
+            console.log("digit pressed");
+        } else if(Buttons.className === "arithmetic"){
+            console.log("arithmetic pressed");
+        }else if(Buttons.id === "clear"){
+            console.log("clear pressed");
+        }else if(Buttons.id === "sign"){
+            console.log("sign pressed");
+        }else if(Buttons.id === "percentage"){
+            console.log("percentage pressed");
+        }
         });
     });
+
+function updateDisplay(userInput){
+    let temp = userInput.toString();
+    const display = document.getElementById("Display");
+        if (display.innerHTML === "0"){
+            display.innerHTML = temp;
+        } else if(display.innerHTML != "0"){
+            display.innerHTML = display.innerHTML + temp;
+        }else if(display.innerHTML === "="){
+            //call function that does arithmetic
+        }
+}
