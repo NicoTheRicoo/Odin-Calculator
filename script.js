709f0e7a965d7aa7198587f66298a2a76097e308
NicoTheRicoo <nicotherico@gmail.com>
2024-07-11 15:53:02 -0400

    base html and css complete

---
+++ script.js
@@ -0,0 +1,11 @@
+let var1 = 0;
+let var2 = 0;
+let sign;
+
+
+const buttons = document.querySelectorAll("button");
+    buttons.forEach((Buttons) => {
+        Buttons.addEventListener("click", () => {
+            
+        });
+    });
