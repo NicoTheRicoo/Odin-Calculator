a5f4eb360afc04779199e4867bb91d1a711d2bde
NicoTheRicoo <nicotherico@gmail.com>
2024-07-11 21:59:32 -0400

    needs test cases now

--- script.js
+++ script.js
@@ -6,37 +6,55 @@ let sign = "";
 //Adding event listeners to all buttons
 const buttons = document.querySelectorAll("button");
     buttons.forEach((Buttons) => {
-        Buttons.addEventListener("click", () => {
+        Buttons.addEventListener("click", () => {               //logs the digit keys pressed
         if(Buttons.className === "digit"){
-            if(sign){
-                var2 = var2 + Buttons.innerHTML;
-                var2.toString();
-                updateDisplay(var2)
+            if(sign){                                           //sign btn pressed
+                var2 = var2 + Buttons.innerHTML;                //concat digits to 2nd var
+                updateDisplay(var2)                             //display 2nd var 
             }else{
-                var1 = var1 + Buttons.innerHTML;
-                var1.toString();
-                updateDisplay(var1);}
-        }else if(Buttons.className === "arithmetic"){
+                var1 = var1 + Buttons.innerHTML;                //no sign btn pressed
+                updateDisplay(var1);}                           
+        }else if(Buttons.className === "arithmetic"){           //assigns arithmetic sign
             sign = Buttons.innerHTML;
-        }else if (Buttons.id === "equals"){
-            if(sign && var1 && var2){
+        }else if (Buttons.id === "equals"){                     //if equals is pressed, calls func
+            if(sign && var1 && var2){                           //must have two var and a sign selected
             Equals(var1, var2, sign);}
-        }else if(Buttons.id === "clear"){
+        }else if(Buttons.id === "clear"){                       //clears and resets vars and sign
             var1 = "";
             var2 = "";
             sign = "";
-            display.innerHTML = 0;
-        }else if(Buttons.id === "sign"){
-            console.log(Buttons.id);
-            console.log("sign pressed");
-        }else if(Buttons.id === "percentage"){
-            console.log("percentage pressed");
+            display.innerHTML = 0;                              //sets display to 0
+        }else if(Buttons.id === "sign"){                        //multiplies by -1
+            if(sign){
+            var2 = SignChange(var2);
+            updateDisplay(var2);
+            } else if (!sign){
+            var1 = SignChange(var1);
+            updateDisplay(var1);
+            }
+        }else if(Buttons.id === "percentage"){                   //divides by 100
+            if(sign){
+                var2 = Percent(var2);
+                updateDisplay(var2);
+                } else if (!sign){
+                var1 = Percent(var1);
+                updateDisplay(var1);
+                }
         }
         });
     });
-    console.log(var1);
-    console.log(var2);
-    console.log(sign);
+
+function Percent(v){
+    v = Number(v);
+    v /= 100;
+    return String(v);
+}
+
+function SignChange(v){
+    v = Number(v);
+    v *= -1;
+    return String(v);
+}
 
 function Equals(v1, v2, s){
     v1 = Number(v1);
